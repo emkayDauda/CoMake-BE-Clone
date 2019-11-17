8a26139c7ed1fa44de94edccffc7aa5e53d09f19
Maaruf Dauda <mkdauda.work@gmail.com>
2019-11-17 22:11:10 +0100

    :construction: Writing out auth tests

---
+++ auth/authModel.spec.js
@@ -0,0 +1,7 @@
+const request = require('supertest')
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+
+beforeEach(() => {
+    return db('users').truncate()
+})
